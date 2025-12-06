1f80ef2b4f3c3a8cac0d948029627920580c3e2b
Soorya <rsoorya003@gmail.com>
2025-12-06 22:11:39 +0530

    resolve port time errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const errorHandler = require('./middleware/errorHandler')
 const connectDb = require('./config/dbConnection')
 const dotenv = require("dotenv").config()
-const port =  5000
+const port =  process.env.PORT || 5000
 const app = express()
 const cors = require('cors')
 app.use(cors())
